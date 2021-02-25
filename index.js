f08093bf07c14d4ed0e9734c676cbc78ed67f012
A F M Muzahidur Rahman <tareq.rahman06@gmail.com>
2021-02-25 12:22:54 +0200

    created simple /api/locatios

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+let database = [
+  { id: 1, latitude: 60, longitude: 70 },
+  { id: 2, latitude: 40, longitude: 80 }
+];
+
 app.use(express.static("public"));
 
-/* app.get("/", (req, res) => {
-  res.send("Hello World");
-}); */
+app.get("/api/locations", (req, res) => {
+  res.json(JSON.stringify(database, null, 4));
+});
 
 app.listen(PORT, () => console.log(`Server started running on port ${PORT}`));
