3a27e1ebc6f63ef763551e8ef8cc048e4124e326
A F M Muzahidur Rahman <tareq.rahman06@gmail.com>
2021-02-25 15:50:30 +0200

    added express router

--- index.js
+++ index.js
@@ -2,16 +2,38 @@ const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+const locationsrouter = express.Router();
+const location = express.Router();
 
 let database = [
   { id: 1, latitude: 60, longitude: 70 },
   { id: 2, latitude: 40, longitude: 80 }
 ];
 
+locationsrouter.get("/location", (req, res) => {
+  res.send("router");
+});
+
+location.get("/locations", (req, res) => {
+  res.json(database);
+});
+
+location.get("/locations/1", (req, res) => {
+  res.json(database[0]);
+});
+
+app.use("/router", locationsrouter);
+app.use("/api", location);
+
 app.use(express.static("public"));
 
-app.get("/api/locations", (req, res) => {
-  res.json(JSON.stringify(database, null, 4));
+/* app.use("/hello", (req, res, next) => {
+  console.log("hello");
+  next();
 });
+app.use("/world", (req, res, next) => {
+  console.log("world");
+  next();
+}); */
 
 app.listen(PORT, () => console.log(`Server started running on port ${PORT}`));
